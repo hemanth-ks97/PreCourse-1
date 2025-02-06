63b1db9b53b08953622ec349f7fd9b3c78a8df72
hemanth-ks97 <hemanth.ks97@gmail.com>
2025-02-05 20:00:04 -0600

    Added python solutions

--- Exercise_2.py
+++ Exercise_2.py
@@ -6,11 +6,23 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head = Node(float('inf'))
         
     def push(self, data):
-        
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
     def pop(self):
+        node = self.head
+        if node.data == float('inf'):
+            return None
         
+        self.head = self.head.next
+        node.next = None
+
+        return node.data
+
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
     
 class SinglyLinkedList:
     def __init__(self):
@@ -10,13 +12,17 @@ class SinglyLinkedList:
         Create a new singly-linked list.
         Takes O(1) time.
         """
-        self.head = None
+        self.head = ListNode(float('inf'))
+        self.tail = self.head
 
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new_node = ListNode(data)
+        self.tail.next = new_node
+        self.tail = self.tail.next
         
     def find(self, key):
         """
@@ -24,9 +30,25 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        cur = self.head.next
+        while cur:
+            if cur.data == key:
+                return cur
+            cur = cur.next
+
+        return None
+
+
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        prev = self.head
+        while prev.next and prev.next.data != key:
+            prev = prev.next
+        
+        cur = prev.next
+        prev.next = cur.next
+        cur.next = None
